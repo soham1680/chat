26b0dc2079ce66caa157b26f9ab25acbea1c40ff
soham1680 <145034426+soham1680@users.noreply.github.com>
2023-09-14 20:41:59 +0530

    Update server.js

--- templatyes/server.js
+++ templatyes/server.js
@@ -8,15 +8,26 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
+const connectedUsers = new Map(); // Map to store connected users and their status
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // When a user connects, add them to the connectedUsers map with an "online" status
+  connectedUsers.set(socket.id, { online: true });
+
+  // Send the list of connected users to all clients
+  io.emit('userList', Array.from(connectedUsers.keys()));
+
   socket.on('message', (message) => {
-    io.emit('message', message); // Broadcast the message to all connected clients
+    io.emit('message', { sender: socket.id, message }); // Include sender's socket.id in the message
   });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    // When a user disconnects, set their status to "offline"
+    connectedUsers.set(socket.id, { online: false });
+    io.emit('userList', Array.from(connectedUsers.keys()));
   });
 });
 
